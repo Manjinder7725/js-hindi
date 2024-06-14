34dd47feb01458997f6490e5074e2a1d9f66630b
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-14 11:22:46 +0000

    done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,26 @@
+const score=400
+// console.log(score)
+
+// const balance = `number: ${score}`
+// console.log(balance);
+
+//++++++++++++++++++++++Maths++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.round(4.5));
+// console.log(Math.round(4.6));
+
+const min=1
+const max=6
+
+// console.log((Math.random()*10+)1)
+
+console.log(Math.floor(Math.random()*(max-min+1))+min) 
+
+console.log(commitandpush)
+console.log(commitandpull)
+
+
