e0ee17a18f49a53d813634544ab43e1712299405
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-15 13:23:30 +0000

    a

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,8 +1,25 @@
 // Array
 
 
-// const myArray= [0,2,4,5]
-// const Heroes=["IRonman","Spiderman","hulk"]
+const myArray= [0,2,4,5]
+const Heroes=["IRonman","Spiderman","hulk"]
 
 
-// console.log(Heroes[1]);
+console.log(Heroes[1]);
+
+//Array methods
+
+myArray.push(7,'yoyo');
+myArray.pop()
+console.log(myArray);
+
+
+const number=[1,2,3,4,5,]
+
+
+console.log(number.slice(0,2))
+
+const num2=[10 ,20 ,30 ,40 ,50]
+
+console.log(num2.splice(1,3));
+console.log(num2);
