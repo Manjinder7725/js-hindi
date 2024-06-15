f2f70babe65cc61db6296a7a0b56b4de0108d1c0
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-15 18:44:06 +0000

    o

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,30 @@
+// console.log("D")
+// console.log("A")
+// console.log("N")
+// console.log("T")
+// console.log("E")
+
+
+function SayMyName(){
+    console.log
+        console.log("D");
+console.log("A");
+console.log("N");
+console.log("T");
+console.log("E");
+    
+}
+
+
+// SayMyName()
+
+function addTwoNumbers(number1,number2){
+
+    console.log(number1+number2);
+
+}
+
+
+addTwoNumbers(4,4)
+
+
