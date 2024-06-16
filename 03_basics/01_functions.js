72dd5e403cf905a5594b0be43cde3b383551238d
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-16 13:57:36 +0000

    f

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,13 +18,82 @@ console.log("E");
 
 // SayMyName()
 
+// function addTwoNumbers(number1,number2){
+
+
+// let result=number1+number2
+// return result
+// }
+
+
+// const result=addTwoNumbers(4,4)
+
+// console.log("result:",result)
+
+
 function addTwoNumbers(number1,number2){
 
-    console.log(number1+number2);
 
-}
+   return number1+number2
+    }
+    
+    
+    const result=addTwoNumbers(4,4)
+    
+    // console.log("result:",result)
+    
+    // function loginUserMessage(username){
+    //     console.log(`${username}  just logged in`)
+    // }
+
+    // loginUserMessage("Dante")
+
+    function userLoginMessage(User){
+        return`${User} just logged in`
+    }
+
+    console.log(userLoginMessage("Vergil"))
 
 
-addTwoNumbers(4,4)
+// ++++++++++++++++++Parametes+++++++++++++++++++
+// jdo pta na hove v kine pararmeters aune ya infinite pararmeter aune odo ki krna xd,
+// dor eg shopping cart di list goes on and on , the customers keep adding to the list in programming languagae they are sending pararmeters,
+    // +++++++++++++++=Shoppimg Cart++++++++++++++++++
+
+    function CalculateCartPrice(...num1){
+
+        return num1
+    }
+
+    console.log((CalculateCartPrice(200,400,500)));
+    // here it is called rest operator
 
 
+    // we have a rest operator to deal with this sceanario  ... is called the rest operator, it is also called the spread operator.It is givethe name according to the usecase. kdo rest  bolna kdo spread bona depends on the usecase.
+
+
+    const person={
+        name:"Kratos",
+        Price:199,
+    }
+
+    function handleobject(anyobject){
+        console.log(`Usernaam is ${anyobject.name} and price is ${anyobject.Price}`)
+    }
+
+
+    handleobject(person) //
+    handleobject({name:"yoyo",
+        Price:399,
+        
+    })
+
+
+    const MyNewArray=[200 ,400, 500]
+
+    function returnSecvalue(Arr){
+    return Arr[1]
+
+    }
+
+    console.log(returnSecvalue(MyNewArray));
