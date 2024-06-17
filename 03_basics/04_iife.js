70a28726182030b74d783085c38306b786053e69
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-17 14:42:30 +0000

    y

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// IIFE is a interesting concept of javascript
+
+// IIFE stand for Immediately Invoked Function Expression
+
+
+// Immediately Invoked Function Expression
+
+// It is used to run/execute the function immediately
+
+
+//  Global scope k pollution se hoti hai problem kyi baar usse hatane k lite liye IIFE is used 
+
+// (()=>console.log("IIFE use krke kita funciton execute"))()
+
+console.log("why")
