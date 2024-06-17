68ec76981fc82d947ea0e50ef05120fb2ea30c9f
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-17 14:27:41 +0000

    arrow func

---
+++ 03_basics/03_ThisAndArrowFunctions.js
@@ -0,0 +1,45 @@
+// Arrow Funtion syntax is () => {}
+
+    // const addtwo = (number2,number1)=>{
+    //     return number1+number2
+    // }
+
+    // console.log(addtwo(5,5))
+
+
+    // const Plus = (numb1,numb2)=>{
+    //     return numb1 + numb2
+    // }
+
+    // console.log(Plus(5,5));
+
+    // console.log("why");
+
+
+    // const yea=()=>{
+    //     console.log("arrow function")
+    // }
+
+    // yea()
+
+    // I think to use arrow fucniton it is neccessary to store it in  a variable
+
+    // so there are other way to write arrow fucnitonsfor eg implisit return
+
+    // implisit return means ek hi line ch return likhna without using curly brackets
+
+    // Implisit mtlb "m(javascript) mnn leta hoon k apne yaha {} lgaye hain"
+
+    let implisit=(digit1,digit2)=> digit1+digit2
+
+    console.log(implisit(6,4))
+
+
+    let example=(ek,doosra)=>  (ek+doosra)
+
+    console.log(example(4,6))
+    
+
+    //  Curly brackets ch wrap kita tn return keyword likhna pyu , j () ch wrap kita tn return likn di jrurat nhi 
+
+    
