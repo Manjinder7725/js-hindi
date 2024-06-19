030fe7626976ce7f66928917b97163f8eb25cade
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-19 18:35:05 +0000

    ty

---
+++ 04_iterations/01_forloops.js
@@ -0,0 +1,30 @@
+//For Loop
+
+// let val= 0;
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(`the value is : ${val++}`);
+    
+// }
+
+
+
+// console.log(2);
+
+//Some keywords down below
+
+
+// Break and Continue
+
+for (let index = 1; index <=20; index++) {
+    console.log(index)
+    
+}
+for (let index = 1; index <=20; index++) {
+    console.log(index)
+    
+}
+for (let index = 1; index <=20; index++) {
+    console.log(index)
+    
+}
