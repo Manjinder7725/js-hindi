7173740de60414779ee180d817e86a412d4d1b50
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-20 11:44:25 +0000

    fg

---
+++ 04_iterations/02_whileandDowhileloop.js
@@ -0,0 +1,27 @@
+// While and Do while loop
+
+let index =0;
+
+
+while (index<=10) {
+    console.log(`While loop excecuted`);
+    index++;
+}
+
+
+let myArray=["yo","hi","hello"]
+let arr=0;
+
+while(arr<myArray.length){
+    console.log(myArray[arr]);
+    arr++;
+}
+
+// ++++++++++++DoWhileLOOP++++++++++++++++++
+
+
+let score=1;
+let num =10
+do{console.log(`score is ${score}`); score =score+2}while(score<num);
+
+// Working code ye
