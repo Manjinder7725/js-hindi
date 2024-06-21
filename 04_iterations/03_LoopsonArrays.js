48cc9c31ce31eeb5d0ce76992bf9df453e3b4d9d
Manjinder7725 <160847871+Manjinder7725@users.noreply.github.com>
2024-06-21 14:06:18 +0000

    dom

--- 04_iterations/03_LoopsonArrays.js
+++
@@ -1,9 +0,0 @@
-// For of Loop
-
-
-const arr=[2,3,4,5]
-
-for (const num of arr) {
-    
-console.log(arr[num])
-}
---
+++ 04_iterations/three.js
@@ -0,0 +1,27 @@
+// For of Loop
+
+
+const arr=[1,2,3,4,5]
+
+for (const num of arr) {
+    
+// console.log(num)
+}
+
+const greeting="hello world";
+
+for (const greet of greeting) {
+    console.log(`Each character is ${greet}`)
+    if(greet==" ")continue;
+    
+    
+}
+
+// +++++++++++++++++++MAPS+++++++++++++++++++++
+
+// Map is a Object. it holds key value pairs ad remember the original insertion ordeer of the keys
+//duplicate value ni hundi MAP ch
+
+
+    
+
